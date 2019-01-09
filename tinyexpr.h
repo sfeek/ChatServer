8e3a880b8a2ca51bc116715c35f2dd24d05718f0
sfeek <shane.feek@gmail.com>
2019-01-09 09:41:27 -0500

    Linted

--- tinyexpr.h
+++ tinyexpr.h
@@ -32,14 +32,21 @@ extern "C" {
 
 
 
-typedef struct te_expr {
+typedef struct te_expr
+{
     int type;
-    union {double value; const double *bound; const void *function;};
+    union
+    {
+        double value;
+        const double *bound;
+        const void *function;
+    };
     void *parameters[1];
 } te_expr;
 
 
-enum {
+enum
+{
     TE_VARIABLE = 0,
 
     TE_FUNCTION0 = 8, TE_FUNCTION1, TE_FUNCTION2, TE_FUNCTION3,
@@ -51,7 +58,8 @@ enum {
     TE_FLAG_PURE = 32
 };
 
-typedef struct te_variable {
+typedef struct te_variable
+{
     const char *name;
     const void *address;
     int type;
@@ -62,21 +70,21 @@ typedef struct te_variable {
 
 /* Parses the input expression, evaluates it, and frees it. */
 /* Returns NaN on error. */
-double te_interp(const char *expression, int *error);
+double te_interp (const char *expression, int *error);
 
 /* Parses the input expression and binds variables. */
 /* Returns NULL on error. */
-te_expr *te_compile(const char *expression, const te_variable *variables, int var_count, int *error);
+te_expr *te_compile (const char *expression, const te_variable *variables, int var_count, int *error);
 
 /* Evaluates the expression. */
-double te_eval(const te_expr *n);
+double te_eval (const te_expr *n);
 
 /* Prints debugging information on the syntax tree. */
-void te_print(const te_expr *n);
+void te_print (const te_expr *n);
 
 /* Frees the expression. */
 /* This is safe to call on NULL pointers. */
-void te_free(te_expr *n);
+void te_free (te_expr *n);
 
 
 #ifdef __cplusplus
